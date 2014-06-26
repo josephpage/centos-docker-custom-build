4690e4042fce1683be1ae78fe4a0a5575a565886
Joseph Page <joseph.page@rednet.io>
2014-06-26 08:22:39 +0200

    echo in english

--- docker-start.sh
+++ docker-start.sh
@@ -2,10 +2,14 @@
 
 cd /root;
 if [ -f /volume/my.ks ]; then
+  echo "Custom Kickstart is detected."
   KS=/volume/my.ks;
 else
+  echo "No custom Kickstart."
   KS=/root/default.ks
 fi
 
+echo "Image compilation..."
 ami-creator $KS $1
+echo "Importing image into Docker..."
 ./img2docker.sh $1.img $1
